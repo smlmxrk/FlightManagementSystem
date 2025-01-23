b738e5e11127cdd2ce7829992abb020859508c6b
sml.mxrk <marktheartest@gmail.com>
2025-01-22 18:02:18 -0600

    gitignore updated + other changes in prep for db

--- src/main/java/mark/main/Application.java
+++ src/main/java/mark/main/Application.java
@@ -1,6 +1,6 @@
 package mark.main;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/main/java/mark/util/DBConnection.java
+++ src/main/java/mark/util/DBConnection.java
@@ -6,4 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/airport";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
 }
