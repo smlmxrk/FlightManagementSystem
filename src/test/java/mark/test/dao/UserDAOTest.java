ed85c7dfff50c2353b8a6d0ab788f4ea5c74a533
sml.mxrk <marktheartest@gmail.com>
2025-02-03 21:07:18 -0600

    user tests started

---
+++ src/test/java/mark/test/dao/UserDAOTest.java
@@ -0,0 +1,11 @@
+package mark.test.dao;
+import mark.dao.UserDAO;
+import mark.util.DBConnection;
+
+import java.util.List;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UserDAOTest {
+}
